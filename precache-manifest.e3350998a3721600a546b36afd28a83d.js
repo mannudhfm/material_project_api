8d9f1722aaa1d77d01cd19fce79074f00b41e4bd
mannudfm <mannudhfm@gmail.com>
2024-01-10 11:41:54 +0530

    Updates

---
+++ precache-manifest.e3350998a3721600a546b36afd28a83d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6380f3edfeccf60bdfe76f9e00199552",
+    "url": "/material_project_api/index.html"
+  },
+  {
+    "revision": "e28695dc1a616820e7a5",
+    "url": "/material_project_api/static/css/main.5934db14.chunk.css"
+  },
+  {
+    "revision": "31e76be5c03afa7101c6",
+    "url": "/material_project_api/static/js/2.8d513cbb.chunk.js"
+  },
+  {
+    "revision": "a85d27c8d9ad00af84305c7e62f8dfbd",
+    "url": "/material_project_api/static/js/2.8d513cbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e28695dc1a616820e7a5",
+    "url": "/material_project_api/static/js/main.5fbccaec.chunk.js"
+  },
+  {
+    "revision": "9d7c3a1159b29d2e98a0",
+    "url": "/material_project_api/static/js/runtime-main.23651e00.js"
+  }
+]);
